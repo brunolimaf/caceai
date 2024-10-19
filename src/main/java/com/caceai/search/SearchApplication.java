ea767277e0b762a79b6298529f673ab153e1613d
brunolima <brunolimadeveloper@gmail.com>
2024-10-19 12:35:46 -0300

    Update PORT

--- src/main/java/com/caceai/search/SearchApplication.java
+++ src/main/java/com/caceai/search/SearchApplication.java
@@ -2,6 +2,9 @@ package com.caceai.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SearchApplication {
@@ -9,6 +12,16 @@ public class SearchApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SearchApplication.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableTomcatWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> {
+			String port = System.getenv("PORT");
+			if (port != null) {
+				factory.setPort(Integer.parseInt(port));
+			}
+		};
 	}
 
 }
