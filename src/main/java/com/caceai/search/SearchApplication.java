6e176c580f90f2e2812dc4add70e5f4617ba6620
brunolima <brunolimadeveloper@gmail.com>
2024-10-20 16:40:05 -0300

    Remove .env from tracking

--- src/main/java/com/caceai/search/SearchApplication.java
+++ src/main/java/com/caceai/search/SearchApplication.java
@@ -1,5 +1,6 @@
 package com.caceai.search;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
